4c299b7ebedf0fb3d8c01aa2b587bc5b55dfdcd9
fukayayuto <fukaya1287@gmail.com>
2020-07-19 14:07:19 +0900

    todoCell追記

--- OriginalTodoApp/Contoroller/TodoViewController.swift
+++ OriginalTodoApp/Contoroller/TodoViewController.swift
@@ -8,15 +8,50 @@
 
 import UIKit
 
-class TodoViewController: UIViewController {
+class TodoViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+   
+    
 
+    @IBOutlet weak var todoTableView: UITableView!
+    
+     var todoTextArray:[String] = ["体操","ランニング"]
+    
+     var todoTimeArray:[String] = ["10:00","09:00"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        todoTableView.register(UINib(nibName: "ToDoCell", bundle: nil),forCellReuseIdentifier:"ToDoCell")
+        todoTableView.delegate = self
+        todoTableView.dataSource = self
     }
     
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+         // #warning Incomplete implementation, return the number of sections
+         return 1
+     }
 
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         // #warning Incomplete implementation, return the number of rows
+         return todoTextArray.count
+     }
+     
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell") as! ToDoCell
+        cell.ToDoTextLabel.text = todoTextArray[indexPath.row]
+        cell.ToDoTimeLabel.text = todoTimeArray[indexPath.row]
+        return cell
+    
+    }
+     
+     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+          return view.frame.size.height/8
+             }
+     
+    
+    
     /*
     // MARK: - Navigation
 
